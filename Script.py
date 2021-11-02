13765c3957a066f5fdc068b214b0707f9174f59f
ThomzEdizon <90401221+ThomzEdizon@users.noreply.github.com>
2021-11-02 17:13:35 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -16,18 +16,13 @@ class script(object):
 ➤𝐁𝐎𝐓 𝐒𝐄𝐑𝐕𝐄𝐑: [𝐇𝐄𝐑𝐎𝐊𝐔](https://www.heroku.com/)
 
 ➤𝐁𝐔𝐈𝐋𝐃 𝐒𝐓𝐀𝐓𝐔𝐒: 𝐯𝟏.𝟎.𝟏 [𝐁𝐄𝐓𝐀]"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+      
+   MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-<b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
-    MANUELFILTER_TXT = """Help: <b>Filters</b>
-
-- Filter is the feature were users can set automated replies for a particular keyword and tessa will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and Natalia will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. eva maria should have admin privillage.
+1. Natalia should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -46,7 +41,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https://t.me/NataliaTG_bot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
