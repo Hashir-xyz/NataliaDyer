c55284d0bf29a455993afbadf6d5663d71d1649c
ThomzEdizon <90401221+ThomzEdizon@users.noreply.github.com>
2021-11-04 16:56:34 +0530

    Create Script.py

--- Script.py
+++ Script.py
@@ -1,9 +1,9 @@
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href='https://t.me/EvaMariaBot'>𝙴𝚅𝙰 𝙼𝙰𝚁𝙸𝙰</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href='https://t.me/NataliaTG_bot'>𝙽𝚊𝚝𝚊𝚒𝚊 𝙳𝚢𝚎𝚛</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """➤ 𝐌𝐘 𝐍𝐀𝐌𝐄  : [𝐍𝐀𝐓𝐀𝐋𝐈𝐀 𝐃𝐘𝐄𝐑](https://t.me/NataliaTG_bot)
+    ABOUT_TXT = """ : [𝐍𝐀𝐓𝐀𝐋𝐈𝐀 𝐃𝐘𝐄𝐑](https://t.me/NataliaTG_bot)
     
 ➤𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑: [𝐄𝐃𝐈𝐙𝐎𝐍](https://t.me/Edizon1)
 
