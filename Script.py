82125093165eda15d503672eb70b85a2c135893b
ThomzEdizon <90401221+ThomzEdizon@users.noreply.github.com>
2021-11-05 10:25:32 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -31,11 +31,11 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Maria Supports both url and alert inline buttons. 
+- Natalia Supports both url and alert inline buttons. 
  
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
+2. Natalia supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
@@ -66,7 +66,7 @@ class script(object):
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of tessa
+these are the extra features of Natalia
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specifed user.</code>
